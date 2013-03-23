dcca8aff07eae7d10f3ec8c7f99c0989aecadaee
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-03-24 00:39:12 +0400

    added osx defaults script

---
+++ osx/set-defaults.sh
@@ -0,0 +1,25 @@
+# Sets reasonable OS X defaults.
+#
+# The original idea were grabbed from:
+#   https://github.com/holman/dotfiles/blob/master/osx/set-defaults.sh
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
