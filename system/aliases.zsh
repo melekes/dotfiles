b578750d007274d24757e2f995d909fd3147e6b4
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-07-09 10:22:40 +0400

    set up vagrant aliases

---
+++ system/aliases.zsh
@@ -0,0 +1,9 @@
+# vagrant aliases:
+alias v="vagrant"
+alias vst="vagrant status"
+alias vup="vagrant up"
+alias vpr="vagrant provision"
+alias vhl="vagrant halt"
+alias vre="vagrant reload"
+alias vssh="vagrant ssh"
+
