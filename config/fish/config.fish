8aec0a3e7604e7047dc68c9dd8b56242b5d7ad75
Anton Kalyaev <a.kalyaev@fun-box.ru>
2015-05-20 09:58:37 +0300

    new oh-my-fish plugins
    exited to try z

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,15 @@
 # Oh My Fish!
 set fish_path $HOME/.oh-my-fish
 set fish_theme l
-set fish_plugins bundler rvm gvm autojump
+set fish_plugins z vi-mode tmux replace extract
 . $fish_path/oh-my-fish.fish
 
 set -U EDITOR vim
 
 # Load scripts from bin
 set PATH $PATH $HOME/.bin
+
+set -U Z_SCRIPT_PATH ~/.plugin_data/z.sh
 
 # aliases
 if test -e ~/.aliases
