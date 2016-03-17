7136d6b0e58120b68f45f3695d857bfc73e65a32
Anton Kalyaev <a.kalyaev@fun-box.ru>
2016-03-17 10:54:17 +0300

    it appears directory is the same for both Mac and Linux

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,15 +7,11 @@ end
 # alias tmux for proper coloring
 alias tmux="env TERM=screen-256color-bce tmux"
 
-switch (uname)
-    case Linux
-            set fisher_home ~/.local/share/fisherman
-    case Darwin
-            set fisher_home ~/.local/share/fisherman
-end
+set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
-set fisher_file ~/.fishfile
 source $fisher_home/config.fish
+
+set -g fisher_file ~/.fishfile
 
 if test -e ~/.config/fish/config.fish.local
   source ~/.config/fish/config.fish.local
