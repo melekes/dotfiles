1760478d27328ecdab01d06f399764a234d28bb9
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-07-08 09:56:38 +0400

    no need for sourcing any more

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,10 +24,6 @@ end
 # alias tmux for proper coloring
 alias tmux="env TERM=xterm-256color tmux"
 
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-
 set -g fisher_file ~/.fishfile
 
 if test -e ~/.config/fish/config.fish.local
